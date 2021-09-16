ffc0aef015190a889634825311094b9140f2a9da
LJH <dlwngh1113@naver.com>
2021-09-16 16:09:37 +0900

    multi thread performance test

--- main.cpp
+++ main.cpp
@@ -1,33 +1,37 @@
 #include<iostream>
 #include<thread>
+#include<mutex>
+#include<chrono>
+#include<vector>
 
 using namespace std;
+using namespace chrono;
 
 int sum{ 0 };
+mutex myLock;
 
-void worker()
+void worker(int threadNum)
 {
-	for (auto i = 0; i < 25'000'000; ++i)
-	{
-		sum += 2;
-		sum += 2;
-		sum += 2;
-		sum += 2;
-		sum += 2;
-		sum += 2;
-		sum += 2;
-		sum += 2;
-		sum += 2;
+	for (auto i = 0; i < 50'000'000 / threadNum; ++i) {
 		sum += 2;
 	}
 }
 
 int main()
 {
-	thread t1{ worker };
-	thread t2{ worker };
-	t1.join();
-	t2.join();
+	const int threadNum = 8;
+	vector<thread> workers;
+	auto beg = high_resolution_clock::now();
+	for (int i = 0; i < threadNum; ++i)
+	{
+		workers.emplace_back(worker , threadNum);
+	}
+	for(auto& t : workers)
+	{
+		t.join();
+	}
+	auto end = high_resolution_clock::now();
+	cout << duration_cast<milliseconds>(end - beg).count() << "msecs\n";
 
 	cout << "Sum = " << sum << endl;
 }
