a5bd12700cfb4dae688fa19b83ca5340991581fe
LJH <dlwngh1113@naver.com>
2021-09-18 16:49:54 +0900

    cache thrashing fix algorithm

--- main.cpp
+++ main.cpp
@@ -8,37 +8,36 @@
 using namespace std;
 using namespace chrono;
 
+const int MAX_THREAD = 16;
 
-atomic <int> sum{ 0 };
+volatile int sum[MAX_THREAD * 16];
 mutex myLock;
 
-void worker(int threadNum)
+void worker(int threadNum, const int thread_id)
 {
 	const int loops = 50'000'000 / threadNum;
-	volatile int sSum{ 0 };
 	for (auto i = 0; i < loops; ++i) {
-		sSum += 2;
+		sum[thread_id * 16] += 2;
 	}
-	sum += sSum;
 }
 
 int main()
 {
-	for (int i = 1; i <= 8; i *= 2)
+	for (int i = 1; i <= MAX_THREAD; i *= 2)
 	{
-		sum = 0;
+		int total_sum{ 0 };
+		for (auto& v : sum)
+			v = 0;
 		vector<thread> workers;
 		auto beg = high_resolution_clock::now();
 		for (int j = 0; j < i; ++j)
-		{
-			workers.emplace_back(worker, i);
-		}
+			workers.emplace_back(worker, i, j);
 		for (auto& t : workers)
-		{
 			t.join();
-		}
+		for (auto& v : sum)
+			total_sum += v;
 		auto end = high_resolution_clock::now();
-		cout << "thread count: " << i << "	milliseconds: " << duration_cast<milliseconds>(end - beg).count() << "msecs";
-		cout << "	Sum = " << sum << endl;
+		cout << "thread count:" << i << "	milliseconds: " << duration_cast<milliseconds>(end - beg).count() << "msecs";
+		cout << "	Sum = " << total_sum << endl;
 	}
 }
