2e9a2e24cde8cbdfa631ddccf09701dbdb115b8c
LJH <dlwngh1113@naver.com>
2021-09-18 16:25:37 +0900

    use local variable

--- main.cpp
+++ main.cpp
@@ -3,35 +3,42 @@
 #include<mutex>
 #include<chrono>
 #include<vector>
+#include<atomic>
 
 using namespace std;
 using namespace chrono;
 
-int sum{ 0 };
+
+atomic <int> sum{ 0 };
 mutex myLock;
 
 void worker(int threadNum)
 {
-	for (auto i = 0; i < 50'000'000 / threadNum; ++i) {
-		sum += 2;
+	const int loops = 50'000'000 / threadNum;
+	volatile int sSum{ 0 };
+	for (auto i = 0; i < loops; ++i) {
+		sSum += 2;
 	}
+	sum += sSum;
 }
 
 int main()
 {
-	const int threadNum = 8;
-	vector<thread> workers;
-	auto beg = high_resolution_clock::now();
-	for (int i = 0; i < threadNum; ++i)
+	for (int i = 1; i <= 8; i *= 2)
 	{
-		workers.emplace_back(worker , threadNum);
+		sum = 0;
+		vector<thread> workers;
+		auto beg = high_resolution_clock::now();
+		for (int j = 0; j < i; ++j)
+		{
+			workers.emplace_back(worker, i);
+		}
+		for (auto& t : workers)
+		{
+			t.join();
+		}
+		auto end = high_resolution_clock::now();
+		cout << "thread count: " << i << "	milliseconds: " << duration_cast<milliseconds>(end - beg).count() << "msecs";
+		cout << "	Sum = " << sum << endl;
 	}
-	for(auto& t : workers)
-	{
-		t.join();
-	}
-	auto end = high_resolution_clock::now();
-	cout << duration_cast<milliseconds>(end - beg).count() << "msecs\n";
-
-	cout << "Sum = " << sum << endl;
 }
