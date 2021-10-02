a9cbaa9141fc47dab6267f7869c3ec523442062e
LJH <dlwngh1113@naver.com>
2021-10-02 16:18:08 +0900

    memory bounding

--- main.cpp
+++ main.cpp
@@ -2,46 +2,34 @@
 
 using namespace std;
 
-const auto SIZE = 50'000'000;
-volatile int x, y;
-int trace_x[SIZE], trace_y[SIZE];
+volatile bool done = false;
+volatile int* bound;
+int error{ 0 };
 
-void ThreadFunc1()
+void Threadfunc1()
 {
-	for (int i = 0; i < SIZE; ++i)
-	{
-		x = i;
-		atomic_thread_fence(memory_order_seq_cst);
-		trace_y[i] = y;
-	}
+	for (int i = 0; i < 25'000'000; ++i)
+		*bound = -(1 + *bound);
+	done = true;
 }
 
-void ThreadFunc2()
+void Threadfunc2()
 {
-	for (int i = 0; i < SIZE; ++i)
+	while (false == done)
 	{
-		y = i;
-		atomic_thread_fence(memory_order_seq_cst);
-		trace_x[i] = x;
+		int v = *bound;
+		if (v != 0 && v != -1)
+			++error;
 	}
 }
 
 int main()
 {
-	thread t1{ ThreadFunc1 };
-	thread t2{ ThreadFunc2 };
+	bound = new int{ 0 };
+	thread t1{ Threadfunc1 };
+	thread t2{ Threadfunc2 };
 	t1.join();
 	t2.join();
-
-	int cnt = 0;
-
-	for (int i = 0; i < SIZE - 1; ++i)
-	{
-		if (trace_x[i] != trace_x[i + 1])continue;
-		int x_value = trace_x[i];
-		if (trace_y[x_value] != trace_y[x_value + 1])continue;
-		if (i != trace_y[x_value])continue;
-		++cnt;
-	}
-	cout << "Total Memory Inconsistency: " << cnt << endl;
+	cout << "error count: " << error << endl;
+	delete bound;
 }
