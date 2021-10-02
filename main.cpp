7285dfe9d5b9e88464069dc43e0ffef8b467302d
LJH <dlwngh1113@naver.com>
2021-10-02 16:07:17 +0900

    atomic_thread_fence add

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ void ThreadFunc1()
 	for (int i = 0; i < SIZE; ++i)
 	{
 		x = i;
+		atomic_thread_fence(memory_order_seq_cst);
 		trace_y[i] = y;
 	}
 }
@@ -20,6 +21,7 @@ void ThreadFunc2()
 	for (int i = 0; i < SIZE; ++i)
 	{
 		y = i;
+		atomic_thread_fence(memory_order_seq_cst);
 		trace_x[i] = x;
 	}
 }
@@ -33,16 +35,13 @@ int main()
 
 	int cnt = 0;
 
-	for (int i = 0; i < SIZE; ++i)
+	for (int i = 0; i < SIZE - 1; ++i)
 	{
-		if (trace_x[i] == trace_x[i + 1])
-		{
-			if (trace_y[trace_x[i]] == trace_y[trace_x[i] + 1])
-			{
-				if (trace_y[trace_x[i]] != i)continue;
-				cnt++;
-			}
-		}
+		if (trace_x[i] != trace_x[i + 1])continue;
+		int x_value = trace_x[i];
+		if (trace_y[x_value] != trace_y[x_value + 1])continue;
+		if (i != trace_y[x_value])continue;
+		++cnt;
 	}
 	cout << "Total Memory Inconsistency: " << cnt << endl;
 }
