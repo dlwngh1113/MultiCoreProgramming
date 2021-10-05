2dc8e8e8f7e2ec12cacd281018850df70c8814fc
LJH <dlwngh1113@naver.com>
2021-10-05 17:05:24 +0900

    atomic & volatile peterson's algorithm

--- main.cpp
+++ main.cpp
@@ -5,15 +5,15 @@ using namespace chrono;
 
 volatile int sum;
 
-atomic< int> flag[2] = { false, false };
-atomic<bool> victim = 0;
+atomic<unsigned char> flag[2] = { 0, 0 };
+volatile int victim = 0;
 
 void p_lock(int my_id)
 {
 	int other = 1 - my_id;
-	flag[my_id] = true;
+	flag[my_id] = 1;
 	victim = my_id;
-	while ((flag[other] == true) && my_id == victim);
+	while ((flag[other].fetch_add(0) == 1) && my_id == victim);
 }
 
 void p_unlock(int my_id)
