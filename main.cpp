f4f38a7140514f0648ff727637743922fef2833a
LJH <dlwngh1113@naver.com>
2021-10-05 15:45:00 +0900

    cache line boundary

--- main.cpp
+++ main.cpp
@@ -5,11 +5,14 @@ using namespace std;
 volatile bool done = false;
 volatile int* bound;
 int error{ 0 };
+mutex l;
 
 void Threadfunc1()
 {
 	for (int i = 0; i < 25'000'000; ++i)
+	{
 		*bound = -(1 + *bound);
+	}
 	done = true;
 }
 
@@ -18,18 +21,24 @@ void Threadfunc2()
 	while (false == done)
 	{
 		int v = *bound;
-		if (v != 0 && v != -1)
+		if (v != 0 && v != -1) {
+			cout << hex << v << '\t';
 			++error;
+		}
 	}
 }
 
 int main()
 {
-	bound = new int{ 0 };
+	int arr[64];
+	long long temp = reinterpret_cast<long long>(arr + 16);
+	temp = temp & 0xFFFFFFFFFFFFFFC0;
+	temp -= 2;
+	bound = reinterpret_cast<int*>(temp);
+	*bound = 0;
 	thread t1{ Threadfunc1 };
 	thread t2{ Threadfunc2 };
 	t1.join();
 	t2.join();
 	cout << "error count: " << error << endl;
-	delete bound;
 }
