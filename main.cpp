af84f81ef0117dc27685135ee8b3fde5bcb51832
LJH <dlwngh1113@naver.com>
2021-10-07 18:53:45 +0900

    fine-grained synchronization

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 using namespace chrono;
 
-constexpr int NUM_TEST = 4000000;
+constexpr int NUM_TEST = 4'000'000;
 constexpr int MAX_THREADS = 8;
 constexpr int NUM_RANGE = 1000;
 
@@ -14,6 +14,16 @@ public:
 	CNode() :value{ 0 }, next{ nullptr }{}
 	CNode(int x) :value{ x }, next{ nullptr } {}
 	virtual ~CNode() {}
+};
+
+class FNode {
+public:
+	int value;
+	FNode* next;
+	mutex nodeLock;
+	FNode() :value{ 0 }, next{ nullptr }{}
+	FNode(int x) :value{ x }, next{ nullptr } {}
+	virtual ~FNode() {}
 };
 
 class nullmutex
@@ -132,12 +142,133 @@ public:
 	}
 };
 
-CList list;
+class FList {
+	FNode head, tail;
+public:
+	FList()
+	{
+		head.value = 0x80000000;
+		tail.value = 0x7FFFFFFF;
+		head.next = &tail;
+	}
+	~FList()
+	{
+		Init();
+	}
+	void Init()
+	{
+		while (head.next != &tail)
+		{
+			FNode* p = head.next;
+			head.next = p->next;
+			delete p;
+		}
+	}
+	bool Add(int x)
+	{
+		FNode* pre, * cur;
+		head.nodeLock.lock();
+		pre = &head;
+
+		pre->next->nodeLock.lock();
+		cur = pre->next;
+		while (cur->value < x)
+		{
+			pre->nodeLock.unlock();
+			pre = cur;
+			cur = cur->next;
+			cur->nodeLock.lock();
+		}
+
+		if (cur->value == x)
+		{
+			pre->nodeLock.unlock();
+			cur->nodeLock.unlock();
+			return false;
+		}
+
+		FNode* nNode = new FNode(x);
+		pre->next = nNode;
+		nNode->next = cur;
+		cur->nodeLock.unlock();
+		pre->nodeLock.unlock();
+		return true;
+	}
+
+	bool Remove(int x)
+	{
+		FNode* pre, * cur;
+		head.nodeLock.lock();
+		pre = &head;
+
+		pre->next->nodeLock.lock();
+		cur = pre->next;
+
+		while (cur->value < x)
+		{
+			pre->nodeLock.unlock();
+			pre = cur;
+			cur = cur->next;
+			cur->nodeLock.lock();
+		}
+
+		if (cur->value == x)
+		{
+			pre->next = cur->next;
+			cur->nodeLock.unlock();
+			pre->nodeLock.unlock();
+			delete cur;
+			return true;
+		}
+
+		cur->nodeLock.unlock();
+		pre->nodeLock.unlock();
+		return false;
+	}
+
+	bool Contains(int x)
+	{
+		FNode* cur;
+		head.nodeLock.lock();
+		cur = &head;
+
+		while (cur->value < x)
+		{
+			cur->nodeLock.unlock();
+			cur = cur->next;
+			cur->nodeLock.lock();
+		}
+
+		if (cur->value == x)
+		{
+			cur->nodeLock.unlock();
+			return true;
+		}
+
+		cur->nodeLock.unlock();
+		return false;
+	}
+	void Print20()
+	{
+		FNode* p = head.next;
+		for (int i = 0; i < 20; ++i)
+		{
+			if (p == &tail)
+				break;
+			cout << p->value << ", ";
+			p = p->next;
+		}
+		cout << endl;
+	}
+};
+
+FList list;
 set<int> s;
 
 void Benchmark(int num_threads)
 {
-	for (int i = 0; i < NUM_TEST / num_threads; ++i)
+	const int loops = NUM_TEST / num_threads;
+	for (int i = 0; i < loops; ++i)
 	{
 		int value = rand() % NUM_RANGE;
 		switch (rand() % 3)
@@ -191,7 +322,7 @@ int main()
 		list.Init();
 		auto beg = high_resolution_clock().now();
 		for(int j = 0;j<i;++j)
-			workers.emplace_back(Benchmark2, i);
+			workers.emplace_back(Benchmark, i);
 		for (auto& t : workers)
 			t.join();
 		auto end = high_resolution_clock().now();
