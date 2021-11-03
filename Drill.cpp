caafff0019433d4b16ac6df2b27303925531054a
LJH <dlwngh1113@naver.com>
2021-11-03 15:12:06 +0900

    wait free algorithm clear

--- Drill.cpp
+++ Drill.cpp
@@ -3,17 +3,24 @@
 #include<vector>
 #include<thread>
 #include<chrono>
+#include<mutex>
+#include<iomanip>
 
-using namespace std;
-using namespace chrono;
+using namespace std::chrono;
 
-constexpr int MAX_THREAD = 8;
+constexpr int MAX_THREAD = 4;
 constexpr int NUM_TEST = 4'000;
 constexpr int NUM_RANGE = 1000;
 
 enum MethodType{DEQUE, ENQUE, CLEAR};
 typedef int InputValue;
 typedef int Response;
+
+std::string enum_str[3] = {
+	"DEQUE",
+	"ENQUE",
+	"CLEAR"
+};
 
 struct Invocation {
 	MethodType type;
@@ -30,10 +37,6 @@ public:
 	{
 		first = response = -1;
 	}
-	bool isUsed()
-	{
-		return response != -1;
-	}
 	Node* Decide(Node*& node)
 	{
 		if (atomic_compare_exchange_strong(&response, &first, reinterpret_cast<int>(node)))
@@ -49,7 +52,10 @@ public:
 	Consensus decideNext;
 	Node* next;
 	volatile int seq;
-	Node() :seq{ 0 }, next{ nullptr }{}
+	Node() :seq{ 0 }, next{ nullptr }
+	{
+		decideNext.Init();
+	}
 	~Node() {}
 	Node(const Invocation& input)
 	{
@@ -93,11 +99,13 @@ class LFUniversal {
 	Node tail;
 	Node* max()
 	{
+		Node* max = head[0];
 		for (int i = 0; i < MAX_THREAD; ++i)
 		{
-			if (!head[i]->decideNext.isUsed())
-				return head[i];
+			if (head[i]->seq > max->seq)
+				max = head[i];
 		}
+		return max;
 	}
 public:
 	LFUniversal()
@@ -162,6 +170,16 @@ public:
 		head[i] = announce[i];
 		return myObject.Apply(cur->invoc);
 	}
+	void print20()
+	{
+		Node* p = tail.next;
+		for (int i = 0; i < 20; ++i)
+		{
+			std::cout << std::setw(10) << enum_str[p->invoc.type] << ", " << p->invoc.value;
+			p = p->next;
+		}
+		std::cout << std::endl;
+	}
 };
 
 LFUniversal lf;
@@ -189,12 +207,41 @@ void Benchmark(int num_threads, int threadID)
 	}
 }
 
+std::mutex sl;
+std::set<int> s;
+
+void Benchmark2(int num_threads, int threadID)
+{
+	for (int i = 0; i < NUM_TEST / num_threads; ++i)
+	{
+		int value = rand() % NUM_RANGE;
+		switch (rand() % 3)
+		{
+		case 0:
+			sl.lock();
+			s.insert(value);
+			sl.unlock();
+			break;
+		case 1:
+			sl.lock();
+			s.erase(value);
+			sl.unlock();
+			break;
+		case 2:
+			sl.lock();
+			s.count(value);
+			sl.unlock();
+			break;
+		}
+	}
+}
+
 int main()
 {
-	for (int i = 1; i < MAX_THREAD; i *= 2)
+	for (int i = 1; i <= MAX_THREAD; i *= 2)
 	{
 		lf.Init();
-		vector<thread> workers;
+		std::vector<std::thread> workers;
 		auto beg = high_resolution_clock().now();
 		for (int j = 0; j < i; ++j)
 			workers.emplace_back(Benchmark, MAX_THREAD, j);
@@ -202,6 +249,7 @@ int main()
 			t.join();
 		auto end = high_resolution_clock().now();
 		auto duration = end - beg;
-		cout << i << " threads, exec time = " << duration_cast<milliseconds>(duration).count() << endl;
+		std::cout << i << " threads, exec time = " << duration_cast<milliseconds>(duration).count() << std::endl;
+		lf.print20();
 	}
 }
