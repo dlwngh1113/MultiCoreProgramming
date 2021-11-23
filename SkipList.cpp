a18bce491ed06b02bfbf28e10502f52918aaf350
LJH <dlwngh1113@naver.com>
2021-11-23 17:23:59 +0900

    Lock Free Skip List

--- SkipList.cpp
+++ SkipList.cpp
@@ -1,11 +1,22 @@
 #include "SkipList.h"
 
+int SkipList::RandomLevel()
+{
+	int new_level = 0;
+	while ((rand() % 2) == 1) {
+		new_level++;
+		if (MAX_LEVEL == new_level)
+			break;
+	}
+	return new_level;
+}
+
 SkipList::SkipList()
 {
 	head.value = 0x80000000;
 	tail.value = 0x7FFFFFFF;
 	for (auto& h : head.next)
-		h = &tail;
+		h.set_ptr(&tail);
 }
 
 SkipList::~SkipList()
@@ -15,168 +26,144 @@ SkipList::~SkipList()
 
 void SkipList::Init()
 {
-	while (head.next[0] != &tail)
+	while (head.next[0].get_ptr() != &tail)
 	{
-		SKNode* p = head.next[0];
-		head.next[0] = p->next[0];
+		LFNode* p = head.next[0].get_ptr();
+		head.next[0].set_ptr(p->next[0].get_ptr());
 		delete p;
 	}
 	for (auto& h : head.next)
-		h = &tail;
+		h.set_ptr(&tail);
 }
 
 bool SkipList::Add(int x)
 {
-	SKNode* pre[MAX_LEVEL + 1], * cur[MAX_LEVEL + 1];
-
-	int nLevel = 0;
-	while (rand() % 2)
-	{
-		++nLevel;
-		if (MAX_LEVEL == nLevel)
-			break;
-	}
+	LFNode* pre[MAX_LEVEL + 1], * cur[MAX_LEVEL + 1];
 
 	while (true)
 	{
-		retry:
-		int fLevel = Find(x, pre, cur);
-		if (fLevel != -1)
+		bool isFound = Find(x, pre, cur);
+		if (isFound)
+			return false;
+
+		int topLevel = RandomLevel();
+		LFNode* nNode = new LFNode(x, topLevel);
+		for (int i = 0; i <= topLevel; ++i)
 		{
-			if (!cur[fLevel]->isMarked)
-			{
-				while (!cur[fLevel]->fullyLinked);
-				return false;
-			}
+			auto suc = cur[i];
+			nNode->next[i].set_ptr(suc);
+		}
+
+		LFNode* pred = pre[0];
+		LFNode* suc = cur[0];
+		nNode->next[0].set_ptr(suc);
+		if (!pred->next[0].CAS(suc, nNode, false, false))
+		{
+			delete nNode;
 			continue;
 		}
-		//노드가 존재하지 않음
-		//locking 시도
-		int maxLockLevel = -1;
-		while(true) {
-			SKNode* pred, * curr;
-			int isValid = true;
-
-			for (int i = 0; i <= nLevel; ++i)
+		for (int i = 1; i <= topLevel; ++i)
+		{
+			while (true)
 			{
 				pred = pre[i];
-				curr = cur[i];
-				pred->nodeLock.lock();
-				maxLockLevel = i;
-				isValid = (!pred->isMarked &&
-					(!curr->isMarked) &&
-					(pred->next[i] == curr));
-				if (!isValid)
-				{
-					for (int i = 0; i <= maxLockLevel; ++i)
-						pre[i]->nodeLock.unlock();
-					goto retry;
-				}
+				suc = cur[i];
+				if (pred->next[i].CAS(suc, nNode, false, false))
+					break;
+				Find(x, pre, cur);
 			}
-			SKNode* nNode = new SKNode(x, nLevel);
-			for (int i = 0; i <= nLevel; ++i)
-				nNode->next[i] = cur[i];
-			for (int i = 0; i <= nLevel; ++i)
-				pre[i]->next[i] = nNode;
-			nNode->fullyLinked = true;
-
-			for (int i = 0; i <= maxLockLevel; ++i)
-				pre[i]->nodeLock.unlock();
-			return true;
 		}
+		return true;
 	}
 }
 
 bool SkipList::Remove(int x)
 {
-	SKNode* pre[MAX_LEVEL + 1], * cur[MAX_LEVEL + 1];
-	SKNode* victim{ nullptr };
-	bool isMarked{ false };
-	int topLevel{ -1 };
+	LFNode* pre[MAX_LEVEL + 1], * cur[MAX_LEVEL + 1];
+
+	if (!Find(x, pre, cur))
+		return false;
+	LFNode* target = cur[0];
+
+	for (int i = target->topLevel; i > 0; --i)
+	{
+		LFNode* suc = target->next[i].get_ptr();
+		while (true)
+		{
+			if (target->next[i].CAS(suc, suc, false, true))
+				break;
+			if (target->next[i].get_removed(suc))
+				break;
+		}
+	}
 
 	while (true)
 	{
-		retry:
-		int lFound = Find(x, pre, cur);
-		if (lFound != -1)
-			victim = cur[lFound];
-		if (isMarked ||
-			((lFound != -1) &&
-			(victim->fullyLinked) &&
-			(victim->topLevel == lFound) &&
-			(!victim->isMarked)))
+		LFNode* suc;
+		bool isMarked = target->next[0].get_removed(suc);
+		if (isMarked)
+			return false;
+		if (target->next[0].CAS(suc, suc, false, true))
 		{
-			if (!isMarked)
-			{
-				topLevel = victim->topLevel;
-				victim->nodeLock.lock();
-				if (victim->isMarked)
-				{
-					victim->nodeLock.unlock();
-					return false;
-				}
-				victim->isMarked = true;
-				isMarked = true;
-			}
-			int highLocked = -1;
-			while(true)
-			{
-				SKNode* pred, * cur;
-				bool valid{ true };
-				for (int i = 0; i <= topLevel; ++i)
-				{
-					pred = pre[i];
-					pred->nodeLock.lock();
-					highLocked = i;
-					valid = !pred->isMarked && (pred->next[i] == victim);
-					if (!valid)
-					{
-						for (int i = 0; i <= highLocked; ++i)
-							pre[i]->nodeLock.unlock();
-						goto retry;
-					}
-				}
-				for (int i = topLevel; i >= 0; --i)
-				{
-					pre[i]->next[i] = victim->next[i];
-				}
-				victim->nodeLock.unlock();
-				for (int i = 0; i <= highLocked; ++i)
-					pre[i]->nodeLock.unlock();
-				return true;
-			}
-		}
-		else
+			Find(x, pre, cur);
 			return true;
+		}
 	}
 }
 
 bool SkipList::Contains(int x)
 {
-	SKNode* pre[MAX_LEVEL + 1], * cur[MAX_LEVEL + 1];
-	int lFound = Find(x, pre, cur);
-	return (lFound != -1 && cur[lFound]->fullyLinked &&
-		!cur[lFound]->isMarked);
+	int bottomLevel{ 0 };
+	bool isMarked = false;
+	LFNode* pre = &head;
+	LFNode* cur = nullptr;
+	LFNode* suc = nullptr;
+	for (int i = MAX_LEVEL; i >= bottomLevel; --i)
+	{	
+		cur = pre->next[i].get_ptr();
+		while (isMarked)
+		{
+			cur = cur->next[i].get_ptr();
+			isMarked = cur->next[i].get_removed(suc);
+		}
+		if (cur->value < x)
+		{
+			pre = cur;
+			cur = suc;
+		}
+		else
+			break;
+	}
+
+	return cur->value == x;
 }
 
-int SkipList::Find(int x, SKNode* pre[], SKNode* cur[])
+bool SkipList::Find(int x, LFNode* pre[], LFNode* cur[])
 {
-	int fLevel = -1;
+retry:
 	int curLevel = MAX_LEVEL;
 	pre[curLevel] = &head;
-
 	while (true)
 	{
-		cur[curLevel] = pre[curLevel]->next[curLevel];
-		while (cur[curLevel]->value < x)
+		while (true)
 		{
+			cur[curLevel] = pre[curLevel]->next[curLevel].get_ptr();
+			LFNode* suc;
+			bool isRemoved = cur[curLevel]->next[curLevel].get_removed(suc);
+			while (isRemoved)
+			{
+				if (!pre[curLevel]->next[curLevel].CAS(cur[curLevel], suc, false, false))
+					goto retry;
+				cur[curLevel] = suc;
+				isRemoved = cur[curLevel]->next[curLevel].get_removed(suc);
+			}
+			if (cur[curLevel]->value >= x)
+				break;
 			pre[curLevel] = cur[curLevel];
-			cur[curLevel] = cur[curLevel]->next[curLevel];
 		}
-		if ((fLevel == -1) && cur[curLevel]->value == x)
-			fLevel = curLevel;
+
 		if (curLevel == 0)
-			return fLevel;
+			return (cur[0]->value == x);
 		--curLevel;
 		pre[curLevel] = pre[curLevel + 1];
 	}
@@ -184,12 +171,12 @@ int SkipList::Find(int x, SKNode* pre[], SKNode* cur[])
 
 void SkipList::Print20()
 {
-	SKNode* p = head.next[0];
+	LFNode* p = head.next[0].get_ptr();
 	for (int i = 0; i < 20; ++i)
 	{
 		if (p == &tail)
 			break;
 		std::cout << p->value << ", ";
-		p = p->next[0];
+		p = p->next[0].get_ptr();
 	}
 }
--- SkipList.h
+++ SkipList.h
@@ -3,30 +3,58 @@
 
 constexpr int MAX_LEVEL = 10;
 
-class SKNode {
+class LFNode;
+
+class MPTR {
+	std::atomic<int> V;
+public:
+	MPTR() : V{ 0 } {}
+	virtual ~MPTR() {}
+
+	void set_ptr(LFNode* p)
+	{
+		V = reinterpret_cast<int>(p);
+	}
+	LFNode* get_ptr()
+	{
+		return reinterpret_cast<LFNode*>(V & 0xFFFFFFFE);
+	}
+	bool set_removed(LFNode*& p, bool b)
+	{
+		int tmpV = reinterpret_cast<int>(p) | 0x01;
+		int curV = reinterpret_cast<int>(p) & 0xFFFFFFFE;
+		return std::atomic_compare_exchange_strong(&V, &curV, tmpV);
+	}
+	bool get_removed(LFNode*& p)
+	{
+		int curV = V;
+		p = reinterpret_cast<LFNode*>(curV & 0xFFFFFFFE);
+		return (curV & 0x1) == 1;
+	}
+	bool CAS(LFNode* old_p, LFNode* new_p, bool old_r, bool new_r)
+	{
+		int old_v = reinterpret_cast<int>(old_p);
+		if (old_r)old_v++;
+		int new_v = reinterpret_cast<int>(new_p);
+		if (new_r)new_v++;
+		return std::atomic_compare_exchange_strong(&V, &old_v, new_v);
+	}
+};
+
+class LFNode {
 public:
 	int value;
-	std::atomic_bool isMarked;
-	std::atomic_bool fullyLinked;
-	std::recursive_mutex nodeLock;
-	SKNode* next[MAX_LEVEL + 1];
+	MPTR next[MAX_LEVEL + 1];
 	int topLevel;
-	SKNode() :value{ 0 }, topLevel{ 0 }, isMarked{ false }, fullyLinked{ false }
-	{
-		for (auto& n : next)
-			n = nullptr;
-	}
-	SKNode(int x, int top) :value{ x }, topLevel{ top }, isMarked{ false }, fullyLinked{ false }
-	{
-		for (auto& n : next)
-			n = nullptr;
-	}
-	virtual ~SKNode() {}
+	LFNode() :value{ 0 }, topLevel{ 0 } {}
+	LFNode(int x, int level) :value{ x }, topLevel{ level } {}
+	virtual ~LFNode() {}
 };
 
 class SkipList
 {
-	SKNode head, tail;
+	LFNode head, tail;
+	int RandomLevel();
 public:
 	SkipList();
 	virtual ~SkipList();
@@ -35,7 +63,7 @@ public:
 	bool Add(int x);
 	bool Remove(int x);
 	bool Contains(int x);
-	int Find(int x, SKNode* pre[], SKNode* cur[]);
+	bool Find(int x, LFNode* pre[], LFNode* cur[]);
 	void Print20();
 };
 
