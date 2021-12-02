06a33832a50b1fbf174d4dabeab4cbc8cadf1b50
LJH <dlwngh1113@naver.com>
2021-12-02 17:09:46 +0900

    tbb task sample

--- main.cpp
+++ main.cpp
@@ -1,44 +1,53 @@
-#include"CList.h"
+#include"stdafx.h"
 
 using namespace std;
 using namespace chrono;
+using namespace tbb::detail::d1;
 
-CList myList;
-
-void Benchmark(int num_threads)
-{
-	for (int i = 0; i < NUM_TEST / num_threads; ++i)
-	{
-		int value = rand() % NUM_RANGE;
-		switch (rand() % 100)
-		{
-		case 0:
-			myList.Add(value);
-			break;
-		case 1:
-			myList.Remove(value);
-			break;
-		default:
-			myList.Contains(value);
-			break;
+class FibTask : public task {
+public:
+	const long n;
+	long* const sum;
+	FibTask(long n_, long* sum_) : n(n_), sum(sum_) {}
+	task* execute() { // Overrides virtual function task::execute
+		if (n <= 2) {
+			*sum = SerialFib(n);
 		}
+		else {
+			long x, y;
+			FibTask& a = *new(allocate_child()) FibTask(n - 1, &x);
+			FibTask& b = *new(allocate_child()) FibTask(n - 2, &y);
+			// Set ref_count to "two children plus one for the wait".
+			set_ref_count(3);
+			// Start b running.
+			spawn(b);
+			// Start a running and wait for all children (a and b).
+			spawn_and_wait_for_all(a);
+			// Do the sum
+			*sum = x + y;
+		}
+		return NULL;
 	}
+};
+
+int SerialFib(int n);
+
+int SerialFib(int n)
+{
+	if (n <= 2)
+		return n;
+	else
+		return SerialFib(n - 1) + SerialFib(n - 2);
 }
 
 int main()
 {
-	for (int i = 1; i <= MAX_THREADS; i *= 2)
-	{
-		vector<thread> workers;
-		myList.Init();
-		auto beg = high_resolution_clock().now();
-		for (int j = 0; j < i; ++j)
-			workers.emplace_back(Benchmark, i);
-		for (auto& t : workers)
-			t.join();
-		auto end = high_resolution_clock().now();
-		myList.Print20();
-		auto duration = end - beg;
-		cout << i << " threads, exec time = " << duration_cast<milliseconds>(duration).count() << endl;
-	}
+	constexpr int F_INDEX = 45;
+	volatile int fibo{ 0 };
+	auto start = system_clock::now();
+	fibo = SerialFib(F_INDEX);
+	auto end = system_clock::now();
+	auto duration = end - start;
+	cout << "single thread fibo[" << F_INDEX << "] - " << fibo;
+	cout << " Exec time = " << duration_cast<milliseconds>(duration).count() << endl;
 }
--- stdafx.h
+++ stdafx.h
@@ -9,11 +9,12 @@
 #include<string>
 #include<omp.h>
 #include<unordered_map>
-//#include<tbb/parallel_for.h>
-//#include<tbb/concurrent_unordered_map.h>
-//#include<tbb/concurrent_hash_map.h>
+#include<tbb/parallel_for.h>
+#include<tbb/concurrent_unordered_map.h>
+#include<tbb/concurrent_hash_map.h>
 #include<shared_mutex>
+#include<tbb/task.h>
 
-constexpr int NUM_TEST = 4'000'000;
+constexpr int NUM_TEST = 5'000'000;
 constexpr int MAX_THREADS = 8;
 constexpr int NUM_RANGE = 1000;
